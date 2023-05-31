339c59d876d63d72ca2802f561ca1286135711e0
Vinicius Gaona <vinicius.gaona@mzgroup.com>
2023-05-30 21:53:39 -0300

    fix on db model

---
+++ prisma/migrations/20230531004950_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Car" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "make" TEXT NOT NULL,
+    "model" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "year" INTEGER NOT NULL
+);
